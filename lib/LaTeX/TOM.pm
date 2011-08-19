22a7314de647da0d646d53b1d97c207a8d0a1574
Steven Schubiger <schubiger@cpan.org>
2011-08-19 17:39:17 +0200

    released 1.01

--- lib/LaTeX/TOM.pm
+++ lib/LaTeX/TOM.pm
@@ -2,7 +2,7 @@
 #
 # LaTeX::TOM (TeX Object Model)
 #
-# Version 1.00_08
+# Version 1.01
 #
 # ----------------------------------------------------------------------------
 #
@@ -33,7 +33,7 @@ use strict;
 use base qw(LaTeX::TOM::Parser);
 use constant true => 1;
 
-our $VERSION = '1.00_08';
+our $VERSION = '1.01';
 
 our (%INNERCMDS, %MATHENVS, %MATHBRACKETS,
      %BRACELESS, %TEXTENVS, $PARSE_ERRORS_FATAL,
